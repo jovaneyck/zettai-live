f6ed4ef839bfb9f39ebf7f9d40cac05c1380c889
Jo Van Eyck <j.vaneyck@farmad.be>
2024-04-02 17:37:51 +0200

    episode 1: walking skeleton

---
+++ Zettai.Tests/AcceptanceTests.fs
@@ -0,0 +1,19 @@
+﻿namespace Zettai.Tests
+
+open Xunit
+open Swensen.Unquote
+open Microsoft.AspNetCore.Hosting
+
+module AcceptanceTests =
+    let buildApp () =
+        new Microsoft.AspNetCore.TestHost.TestServer(new WebHostBuilder() |> Zettai.configure)
+
+
+    [<Fact>]
+    let hello_world () =
+        let app = buildApp ()
+        let client = app.CreateClient()
+
+        let result = client.GetAsync("\hello").Result
+        let content = (result.Content.ReadAsStringAsync()).Result
+        test <@ content = "Hello world!" @>
