5389db6b5b26df38d549881f5616445239659cd8
Jo Van Eyck <j.vaneyck@farmad.be>
2024-04-14 20:23:21 +0200

    add tests to sln

---
+++ Zettai.Tests/SlowTests.fs
@@ -0,0 +1,64 @@
+﻿module Zettai.SlowTests
+
+open Xunit
+open Swensen.Unquote
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.TestHost
+open System.Text.Json
+
+[<Fact>]
+let ``App bootstraps correctly`` () =
+    let whb =
+        new WebHostBuilder()
+        |> Zettai.configure (fun _ -> [])
+
+    let server = new TestServer(whb)
+    let client = server.CreateClient()
+
+    let response =
+        task {
+            let! response = client.GetAsync(@"\")
+            let! content = response.Content.ReadAsStringAsync()
+            return content
+        }
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    test <@ response = "Hello Zettai user!" @>
+
+[<Fact>]
+let ``Can get all lists of a user`` () =
+    let lists: Domain.TodoList list =
+        [ { name = "books"
+            description = "my bookshelf"
+            status = Domain.Status.Todo
+            percentageDone = 0m } ]
+
+    let db = [ ("jo", lists) ] |> Map.ofList
+
+    let mapFetcher username = db |> Map.find username
+
+    let whb =
+        new WebHostBuilder()
+        |> Zettai.configure mapFetcher
+
+    let server = new TestServer(whb)
+    let client = server.CreateClient()
+
+    let response =
+        task {
+            let! response = client.GetAsync(@"\jo\lists")
+            test <@ response.StatusCode = System.Net.HttpStatusCode.OK @>
+            let! stream = response.Content.ReadAsStreamAsync()
+            let! content = JsonSerializer.DeserializeAsync<Contract.GetListsResponse>(stream, jsonOptions)
+            return content
+        }
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+    test
+        <@ response = { lists =
+                            [| { name = "books"
+                                 description = "my bookshelf"
+                                 status = Contract.Status.Todo
+                                 percentageDone = 0m } |] } @>
